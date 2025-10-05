3a522068772a9332c07b198346966275ea5e6573
Hugo Burton <hugo.burton@westpac.com.au>
2025-10-05 19:01:07 +1000

    feat: signature help

--- init.lua
+++ init.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
   "hrsh7th/cmp-nvim-lsp",
   "hrsh7th/cmp-buffer",
   "hrsh7th/cmp-path",
+  "hrsh7th/cmp-nvim-lsp-signature-help",
   "L3MON4D3/LuaSnip",
   "saadparwaiz1/cmp_luasnip",
 
@@ -266,6 +267,7 @@ cmp.setup({
   }),
   sources = {
     { name = "nvim_lsp" },
+    { name = "nvim_lsp_signature_help" },
     { name = "buffer" },
     { name = "path" },
     { name = "luasnip" },
