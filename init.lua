aafa8c4d86629e8dc7351c98687166cd0118f3bc
Hugo Burton <hugo.burton@westpac.com.au>
2025-10-07 20:41:22 +1000

    feat: debug config

--- init.lua
+++ init.lua
@@ -76,6 +76,76 @@ require("lazy").setup({
     },
   },
 
+  -- Debug config
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "nvim-neotest/nvim-nio" },
+      },
+      "theHamsta/nvim-dap-virtual-text",
+      "jay-babu/mason-nvim-dap.nvim",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      require("mason-nvim-dap").setup({
+        ensure_installed = { "node2" },
+      })
+
+      dapui.setup()
+      require("nvim-dap-virtual-text").setup()
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      -- Node adapter
+      dap.adapters.node2 = {
+        type = "executable",
+        command = "node",
+        args = {
+          vim.fn.stdpath("data") .. "/mason/packages/node-debug2-adapter/out/src/nodeDebug.js",
+        },
+      }
+
+      dap.configurations.javascript = {
+        {
+          name = "Launch current file",
+          type = "node2",
+          request = "launch",
+          program = "${file}",
+          cwd = vim.fn.getcwd(),
+          sourceMaps = true,
+          protocol = "inspector",
+          console = "integratedTerminal",
+        },
+        {
+          name = "Attach to process",
+          type = "node2",
+          request = "attach",
+          processId = require("dap.utils").pick_process,
+        },
+      }
+      dap.configurations.typescript = dap.configurations.javascript
+
+      -- Keymaps
+      vim.keymap.set("n", "<F5>", dap.continue)
+      vim.keymap.set("n", "<F10>", dap.step_over)
+      vim.keymap.set("n", "<F11>", dap.step_into)
+      vim.keymap.set("n", "<F12>", dap.step_out)
+      vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+    end,
+  },
+
   -- Extra essentials
   {
     "folke/which-key.nvim",
