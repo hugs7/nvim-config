005cf487b128e0c214e12c0cba88f2b262956972
Hugo Burton <hugo.burton@westpac.com.au>
2026-01-12 11:26:16 +1000

    fix: find hidden files in telescope

--- lua/hugo/ui/telescope.lua
+++ lua/hugo/ui/telescope.lua
@@ -1,4 +1,18 @@
 -- =========================
+-- Telescope Setup
+-- =========================
+require('telescope').setup({
+  defaults = {
+    file_ignore_patterns = { "node_modules", ".git/" },
+  },
+  pickers = {
+    find_files = {
+      hidden = true,  -- Show hidden files (files starting with .)
+    },
+  },
+})
+
+-- =========================
 -- Telescope keymaps
 -- =========================
 local builtin = require("telescope.builtin")
