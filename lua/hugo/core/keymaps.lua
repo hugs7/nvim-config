5a4754d4f93c8f23c60aa2718493d266c84183fc
Hugo Burton <hugo.burton@westpac.com.au>
2026-01-21 02:04:41 +1000

    feat: react rafc snippet keymap

--- lua/hugo/core/keymaps.lua
+++ lua/hugo/core/keymaps.lua
@@ -11,20 +11,36 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 -- =========================
 
 -- Ctrl+Backspace → delete previous word
-vim.keymap.set("i", "<C-H>", "<C-w>", { noremap = true })
+vim.keymap.set("i", "<C-H>", "<C-w>", {
+  noremap = true
+})
 
 -- Ctrl+Delete → delete next word
-vim.keymap.set("i", "<C-Del>", "<C-o>de", { noremap = true })
+vim.keymap.set("i", "<C-Del>", "<C-o>de", {
+  noremap = true
+})
 
 -- Focus editor
-vim.keymap.set("n", "<leader>ee", "<C-w>l", { noremap = true, silent = true, desc = "Focus editor" })
+vim.keymap.set("n", "<leader>ee", "<C-w>l", {
+  noremap = true,
+  silent = true,
+  desc = "Focus editor"
+})
 
 -- Find file by name
-vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true, silent = true, desc = "Quick Open" })
+vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", {
+  noremap = true,
+  silent = true,
+  desc = "Quick Open"
+})
 
 -- Adjust Nvim Tree Width
-vim.keymap.set("n", "<leader>=", "<cmd>vertical resize +5<CR>", { desc = "Increase NvimTree width" })
-vim.keymap.set("n", "<leader>-", "<cmd>vertical resize -5<CR>", { desc = "Decrease NvimTree width" })
+vim.keymap.set("n", "<leader>=", "<cmd>vertical resize +5<CR>", {
+  desc = "Increase NvimTree width"
+})
+vim.keymap.set("n", "<leader>-", "<cmd>vertical resize -5<CR>", {
+  desc = "Decrease NvimTree width"
+})
 
 -- Copy open file path
 vim.keymap.set('n', '<leader>cp', function()
@@ -36,7 +52,9 @@ end)
 
 vim.keymap.set("n", "<leader>rr", function()
   vim.cmd("restart")
-end, { desc = "Restart Neovim and restore buffers" })
+end, {
+  desc = "Restart Neovim and restore buffers"
+})
 
 -- Braile--
 local is_braille_active = false
@@ -51,31 +69,55 @@ vim.keymap.set("n", "<leader>br", function()
   end
 
   is_braille_active = not is_braille_active
-end, { desc = "Toggle Braille overlay" })
+end, {
+  desc = "Toggle Braille overlay"
+})
 
 -- =========================
 -- TypeScript Keymaps
 -- =========================
 local typescript = require("hugo.langs.typescript")
-vim.keymap.set("n", "<leader>fi", typescript.fix_imports_sequential, { desc = "Fix imports (remove unused + organize)" })
+vim.keymap.set("n", "<leader>fi", typescript.fix_imports_sequential, {
+  desc = "Fix imports (remove unused + organize)"
+})
 
--- =========================
--- React Component Creation
--- =========================
+-- =============
+-- React Keymaps
+-- =============
 local react_components = require("hugo.utils.react-components")
 local react_component_test = require("hugo.utils.react-component-test")
 local react_hooks = require('hugo.utils.react-hooks')
 local react_providers = require('hugo.utils.react-providers')
 local barrel = require('hugo.utils.barrel')
 
-vim.keymap.set("n", "<leader>rc", react_components.create_component, { desc = "Create React component" })
-vim.keymap.set("n", "<leader>rct", react_components.create_component_with_types,
-  { desc = "Create React component with types" })
-vim.keymap.set("n", "<leader>rcv", react_component_test.generate_react_test, { desc = "Create React component test" })
+vim.keymap.set("n", "<leader>rafc", react_components.insert_afc, {
+  desc = "Insert React function component"
+})
+vim.keymap.set("n", "<leader>rafct", react_components.insert_afc_with_types, {
+  desc = "Insert React function component with types"
+})
+vim.keymap.set("n", "<leader>rc", react_components.create_component, {
+  desc = "Create React component"
+})
+vim.keymap.set("n", "<leader>rct", react_components.create_component_with_types, {
+  desc = "Create React component with types"
+})
 
-vim.keymap.set("n", "<leader>rh", react_hooks.create_hook, { desc = "Create React hook" })
-vim.keymap.set("n", "<leader>rht", react_hooks.create_hook_with_types, { desc = "Create React hook with types" })
+vim.keymap.set("n", "<leader>rcv", react_component_test.generate_react_test, {
+  desc = "Create React component test"
+})
 
-vim.keymap.set("n", "<leader>rp", react_providers.create_provider, { desc = "Create React Provider" })
+vim.keymap.set("n", "<leader>rh", react_hooks.create_hook, {
+  desc = "Create React hook"
+})
+vim.keymap.set("n", "<leader>rht", react_hooks.create_hook_with_types, {
+  desc = "Create React hook with types"
+})
 
-vim.keymap.set("n", "<leader>bf", barrel.generate_barrel_export, { desc = "Generate barrel file" })
+vim.keymap.set("n", "<leader>rp", react_providers.create_provider, {
+  desc = "Create React Provider"
+})
+
+vim.keymap.set("n", "<leader>bf", barrel.generate_barrel_export, {
+  desc = "Generate barrel file"
+})
